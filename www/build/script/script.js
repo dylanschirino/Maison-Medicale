86d054ce589739ee064475ba5335ea1dc0216bf8
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-11-16 15:09:31 +0100

    Add dropdown menu with js

--- www/build/script/script.js
+++ www/build/script/script.js
@@ -4,20 +4,35 @@
 
   // Quand le javascript est désactiver
   var body = document.getElementsByTagName("body")[0],
-      searchLink = document.querySelector( ".menu__link--search" ),
-      searchBox = document.querySelector( ".menu__search" ),
-      burger = document.querySelector("#header__burger"),
-      sitecache = document.querySelector("#site-cache");
+  searchLink = document.querySelector( ".menu__link--search" ),
+  searchBox = document.querySelector( ".menu__search" ),
+  burger = document.querySelector("#header__burger"),
+  sitecache = document.querySelector("#site-cache"),
+  OpenInMenu = document.querySelectorAll(".menu__element--down"),
+  subLink = document.querySelectorAll(".menu__sublist");
+  console.log(subLink);
 
   body.classList.remove("no-js");
+
+    if ( window.innerWidth > 1097) {
+      OpenInMenu.forEach(function(element) {
+        element.addEventListener("click",function(e){
+          e.preventDefault();
+          e.target.nextElementSibling.classList.toggle('menu__sublist--open');
+        });
+      });
+    }
 
   window.addEventListener("load",function(){
 
     // Affichage de la barre de recherche
     searchLink.addEventListener("click",function(){
-    searchBox.classList.toggle('menu__search--open');
+      searchBox.classList.toggle('menu__search--open');
 
     });
+
+
+
 
     // Menu burger responsive
     burger.addEventListener("click",function(e){
