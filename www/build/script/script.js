ae7181ef3ac6aa35a2df7f810c612b6000801ae7
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-11-16 11:20:40 +0100

    Add menu burger in javascript

--- www/build/script/script.js
+++ www/build/script/script.js
@@ -1,13 +1,35 @@
 ( function (){
   "use strict";
 
+
+  // Quand le javascript est désactiver
+  var body = document.getElementsByTagName("body")[0],
+      searchLink = document.querySelector( ".menu__link--search" ),
+      searchBox = document.querySelector( ".menu__search" ),
+      burger = document.querySelector("#header__burger"),
+      sitecache = document.querySelector("#site-cache");
+
+  body.classList.remove("no-js");
+
   window.addEventListener("load",function(){
-    document.querySelector( ".menu__link--search" ).addEventListener("click",function(){
-    document.querySelector( ".menu__search" ).classList.toggle('menu__search--open');
+
+    // Affichage de la barre de recherche
+    searchLink.addEventListener("click",function(){
+    searchBox.classList.toggle('menu__search--open');
 
     });
-    var body = document.getElementsByTagName("body")[0];
-    body.classList.remove("no-js");
+
+    // Menu burger responsive
+    burger.addEventListener("click",function(e){
+      e.preventDefault();
+      body.classList.toggle('with--sidebar');
+
+    });
+
+    sitecache.addEventListener("click", function(e){
+      body.classList.remove('with--sidebar')
+    })
+
   });
 
 
