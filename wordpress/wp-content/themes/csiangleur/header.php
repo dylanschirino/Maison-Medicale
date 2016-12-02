0661b64d53dbfd3597e8fc4a2e6667578e839e9a
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-12-02 14:56:57 +0100

    Adding wp_head

--- wordpress/wp-content/themes/csiangleur/header.php
+++ wordpress/wp-content/themes/csiangleur/header.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title><?php echo the_title();?> - <?php echo bloginfo();?></title>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri().'/css/main.css';?>" type="text/css">
+    <?php wp_head(); ?> 
   </head>
   <body class="no-js">
     <div class="site-container">
