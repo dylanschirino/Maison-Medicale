7b854dd1624cb683cc95aecb028ede4d0cebdab8
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-12-03 11:51:05 +0100

    Fixing one error with h1 title

--- wordpress/wp-content/themes/csiangleur/header.php
+++ wordpress/wp-content/themes/csiangleur/header.php
@@ -8,11 +8,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title><?php echo the_title();?> - <?php echo bloginfo();?></title>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri().'/css/main.css';?>" type="text/css">
-    <?php wp_head(); ?> 
+    <?php wp_head(); ?>
   </head>
   <body class="no-js">
     <div class="site-container">
       <div class="site-pusher">
         <header class="header">
           <div class="header__content">
-            <h1 class="header__logo"><a class="header__logolink" href="<?php echo the_permalink('5');?>"><img class="header__img" src="<?php echo get_template_directory_uri().'/images/svg/logo.svg';?>" width="121" height="72" alt="Logo du csi angleur"><span class="hidden">CSI Angleur - Accueil</span></a></h1>
+            <h1 class="header__logo"><a class="header__logolink" href="<?php echo the_permalink('5');?>"><img class="header__img" src="<?php echo get_template_directory_uri().'/images/svg/logo.svg';?>" width="121" height="72" alt="CSI"><span class="hidden"> Angleur - <?php the_title();?></span></a></h1>
